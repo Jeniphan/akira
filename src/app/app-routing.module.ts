911e7852a1633667bae262f176c1cb5c514fddb5
Jetniphan Pukkham <jeni8399@gmail.com>
2021-11-30 11:20:19 +0700

    v0.1.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,8 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     useHash: false,
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
+    anchorScrolling: 'enabled',
   })],
   exports: [RouterModule]
 })
